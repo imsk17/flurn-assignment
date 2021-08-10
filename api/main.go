5123836d8d3e31d4aa0553d84013d55efce67e5b
imsk17 <imsumitkumar17@gmail.com>
2021-08-11 01:37:23 +0530

    feat(db): Setup Manual Migration for our enum type

--- api/main.go
+++ api/main.go
@@ -5,7 +5,8 @@ import (
 	"assignment/pkg/entities"
 	"assignment/pkg/loan"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+	fiberLogger "github.com/gofiber/fiber/v2/middleware/logger"
+	"gorm.io/gorm/logger"
 	"log"
 
 	"gorm.io/driver/postgres"
@@ -14,15 +15,22 @@ import (
 
 func main() {
 	app := fiber.New()
-	app.Use(logger.New())
+	app.Use(fiberLogger.New())
 	db, err := gorm.Open(postgres.New(
 		postgres.Config{
 			DSN: "postgres://postgres:postgres@localhost/assignment",
 		},
-	))
+	), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		log.Panicf("Failed to connect to the database: %e", err)
 	}
+	// Our ORM right now does not support auto migration
+	// for enum types and hence we have to migrate it ourselves.
+	//! IMPORTANT: Should be run only once
+	_ = db.Exec("CREATE TYPE status AS ENUM ('New', 'Approved', 'Rejected', 'Cancelled')").Error
+
 	if db.AutoMigrate(&entities.LoanRequest{}) != nil {
 		log.Panicf("Failed to migrate the database: %e", err)
 	}
